af7b9ae9089f741ab90fb6bc4a9d81f60287a858
CardboardThinker <91638086+CardboardThinker@users.noreply.github.com>
2021-10-04 03:16:54 +0300

    Add files via upload

---
+++ Лабораторная работа 6 задания 1-4/лабраб6задание1/лабраб6задание1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float A, B;
+	cout << "введите две переменных A и B, а программа поменяет их значения местами" << endl;
+	cout << "введите A" << endl;
+	cin >> A;
+	cout << "введите B" << endl;
+	cin >> B;
+	swap(A,B);
+	cout << "Теперь А = " << A << endl;
+	cout << "Теперь B = " << B << endl;
+	return 0;
+}
---
+++ Лабораторная работа 6 задания 1-4/лабраб6задание3/лабраб6задание3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float A, B, C;
+	cout << "введите три переменных A, B  и С, а программа поменяет местами значения A и С, С и B, B и A" << endl;
+	cout << "введите A" << endl;
+	cin >> A;
+	cout << "введите B" << endl;
+	cin >> B;
+	cout << "введите С" << endl;
+	cin >> C;
+	swap(A, C);
+	swap(A, B);
+	cout << "Теперь А = " << A << endl;
+	cout << "Теперь B = " << B << endl;
+	cout << "Теперь C = " << C << endl;
+	return 0;
+}
