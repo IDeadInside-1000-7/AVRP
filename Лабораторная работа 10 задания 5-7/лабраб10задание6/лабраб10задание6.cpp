091c393149a97cea8e3781d99968742b97d2f6d3
CardboardThinker <91638086+CardboardThinker@users.noreply.github.com>
2021-10-10 21:34:21 +0300

    Add files via upload

---
+++ Лабораторная работа 10 задания 5-7/лабраб10задание6/лабраб10задание6.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b, c;
+	cout << "Введите три стороны треугольника: a, b, c, чтобы проверить истинность высказывания: треугольник со сторонами a, b, c является прямоугольным" << endl;
+	cout << "введите a" << endl;
+	cin >> a;
+	cout << "введите b" << endl;
+	cin >> b;
+	cout << "введите c" << endl;
+	cin >> c;
+	if (a > b && a > c) {
+		if (pow (a, 2) == pow(b, 2) + pow(c, 2)) {
+			cout << "Высказывание верно" << endl;
+		}
+	else {
+		cout << "Высказывание неверно" << endl;
+	}
+	}
+	if (b > a && b > c) {
+		if (pow(b, 2) == pow(a, 2) + pow(c, 2)) {
+			cout << "Высказывание верно" << endl;
+		}
+		else {
+			cout << "Высказывание неверно" << endl;
+		}
+	}
+	if (c > a && c > b) {
+		if (pow(c, 2) == pow(a, 2) + pow(b, 2)) {
+			cout << "Высказывание верно" << endl;
+		}
+		else {
+			cout << "Высказывание неверно" << endl;
+		}
+	}
+
+
+	return 0;
+}
---
+++ Лабораторная работа 10 задания 5-7/лабраб10задание7/лабраб10задание7.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b, c;
+	cout << "Введите три стороны треугольника: a, b, c, чтобы проверить истинность высказывания: треугольник со сторонами a, b, c существует" << endl;
+	cout << "введите a" << endl;
+	cin >> a;
+	cout << "введите b" << endl;
+	cin >> b;
+	cout << "введите c" << endl;
+	cin >> c;
+	if (a < b+c && c < a+b && b < a+c) {
+		cout << "Высказывание верно" << endl;
+	}
+    else {
+		cout << "Высказывание неверно" << endl;
+	}
+	return 0;
+}
