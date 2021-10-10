3e240621e889a734192b09e4ecc9df858bb35406
CardboardThinker <91638086+CardboardThinker@users.noreply.github.com>
2021-10-10 21:29:09 +0300

    Add files via upload

---
+++ Лабораторная работа 9/лабраб9задание1/лабраб9задание1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int N;
+	cout << "С начала суток прошло N секунд (N — целое). Введите N, чтобы найти количество секунд, прошедших с начала последней минуты." << endl;
+checkN:
+	cin >> N;
+	if (N < 0) {
+		cout << "N должно быть целым положительным числом" << endl;
+		goto checkN;
+	}
+	
+	N = N % 60;
+	cout << "Количество секунд прошедших с последней минуты: " << N << endl;
+	return 0;
+}
