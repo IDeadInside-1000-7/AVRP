1e784d7e68965d4651ae1a2a5ce2a2956045aa24
IDeadInside-1000-7 <90912641+IDeadInside-1000-7@users.noreply.github.com>
2021-10-12 14:56:12 +0300

    Update лабраб8задание1.cpp

--- Лабораторная работа 8/лабраб8задание1/лабраб8задание1.cpp
+++ Лабораторная работа 8/лабраб8задание1/лабраб8задание1.cpp
@@ -1,12 +1,14 @@
-﻿#include <iostream>
+#include <iostream>
+#include <cmath>
 using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	float a;
+	float a,b;
 	cout << "введите число байт, для расчета целого числа килобайт" << endl;
 	cin >> a;
-	int b=a/1024;
+	b=a/1024;
+	b= ceil (b);
 	cout << "Целых килобайт: " << b << endl;
 	return 0;
 }
